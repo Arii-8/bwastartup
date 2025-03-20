05196e7a62cffce771b033f472209ee899a051c2
Arii-8 <wilsontsukatao64@gmail.com>
2025-03-21 05:29:47 +0700

    add fetch user

--- handler/user.go
+++ handler/user.go
@@ -188,3 +188,13 @@ func (h *userHandler) UploadAvatar(c *gin.Context) {
 
 	c.JSON(http.StatusOK, response)
 }
+
+// Fungsi FetchUser
+func (h *userHandler) FetchUser(c *gin.Context) {
+	currentUser := c.MustGet("currentUser").(user.User)
+	formatter := user.FormatUser(currentUser, "")
+	response := helper.APIResponse("Successfuly fetsh user data", http.StatusOK, "success", formatter)
+
+	// response
+	c.JSON(http.StatusOK, response)
+}
